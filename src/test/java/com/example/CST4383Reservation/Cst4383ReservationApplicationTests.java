5c48e62323699c1e684b0946bece270a40aa8f0d
Michael <Michaelecisneros@gmail.com>
2021-05-16 22:29:07 +0530

    Initial Commit

---
+++ src/test/java/com/example/CST4383Reservation/Cst4383ReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CST4383Reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cst4383ReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
